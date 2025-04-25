45478a8f75c6540f6b0f9223de969051d9962c89
Soumyojyoti Saha <110711548+Soumyojyotisaha@users.noreply.github.com>
2025-04-25 21:30:52 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Landing Page
 import Landing from "./assets/Landing";
+
+// Customer Pages
 import CustomerSignup from "./pages/Customer_Pages/CustomerSignup";
-import SupplierSignup from "./pages/Supplier_Pages/SupplierSignup";
 import CustomerLogin from "./pages/Customer_Pages/CustomerLogin";
-import SupplierLogin from "./pages/Supplier_Pages/SupplierLogin";
 import CustomerDashboard from "./pages/Customer_Pages/CustomerDashboard";
 import ViewOrders from "./pages/Customer_Pages/ViewOrders";
 import PlaceOrder from "./pages/Customer_Pages/PlaceOrder";
@@ -11,6 +14,10 @@ import CancelOrder from "./pages/Customer_Pages/CancelOrder";
 import DeleteProfile from "./pages/Customer_Pages/DeleteProfile";
 import UpdateCustomer from "./pages/Customer_Pages/UpdateCustomer";
 import ForgotPassword from "./pages/Customer_Pages/ForgotPassword";
+
+// Supplier Pages
+import SupplierSignup from "./pages/Supplier_Pages/SupplierSignup";
+import SupplierLogin from "./pages/Supplier_Pages/SupplierLogin";
 import SupplierDashboard from "./pages/Supplier_Pages/SupplierDashboard";
 import GetAllOrders from "./pages/Supplier_Pages/GetAllOrders";
 import UpdateOrderStatus from "./pages/Supplier_Pages/UpdateOrderStatus";
@@ -30,23 +37,29 @@ function App() {
         backgroundImage: "url('/pixelcut-export.png')",
         backgroundSize: "cover",
         backgroundPosition: "center",
-        height: "100vh",
+        backgroundAttachment: "fixed",
+        minHeight: "100vh",
       }}
     >
       <Router>
         <Routes>
+          {/* Landing */}
           <Route path="/" element={<Landing />} />
+
+          {/* Customer Routes */}
           <Route path="/customer-signup" element={<CustomerSignup />} />
-          <Route path="/supplier-signup" element={<SupplierSignup />} />
-          <Route path="/customer-dashboard" element={<CustomerDashboard />} />
           <Route path="/customer-login" element={<CustomerLogin />} />
-          <Route path="/supplier-login" element={<SupplierLogin />} />
+          <Route path="/customer-dashboard" element={<CustomerDashboard />} />
           <Route path="/view-orders" element={<ViewOrders />} />
           <Route path="/place-order" element={<PlaceOrder />} />
           <Route path="/cancel-order" element={<CancelOrder />} />
           <Route path="/delete-profile" element={<DeleteProfile />} />
           <Route path="/update-customer" element={<UpdateCustomer />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
+
+          {/* Supplier Routes */}
+          <Route path="/supplier-signup" element={<SupplierSignup />} />
+          <Route path="/supplier-login" element={<SupplierLogin />} />
           <Route path="/supplier-dashboard" element={<SupplierDashboard />} />
           <Route path="/get-all-order-details" element={<GetAllOrders />} />
           <Route path="/update-order-status" element={<UpdateOrderStatus />} />
